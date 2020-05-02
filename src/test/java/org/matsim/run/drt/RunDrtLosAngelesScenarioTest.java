5c4678a5574cb62bbec897804393798fe66b684d
ikaddoura <kaddoura@vsp.tu-berlin.de>
2020-05-02 11:40:39 +0200

    add config file for reduced scenario, mainly for testing purposes; adjust test to run for smaller network

--- src/test/java/org/matsim/run/drt/RunDrtLosAngelesScenarioTest.java
+++ src/test/java/org/matsim/run/drt/RunDrtLosAngelesScenarioTest.java
@@ -43,7 +43,7 @@ public class RunDrtLosAngelesScenarioTest {
 	@Test
 	public final void test1() {
 		try {			
-			String[] args = new String[] { "./scenarios/los-angeles-v1.1/input/drt/los-angeles-drt-v1.1-1pct.config.xml" };
+			String[] args = new String[] { "./scenarios/los-angeles-v1.1/input/drt/los-angeles-WSC-reduced-drt-v1.1-1pct.config.xml" };
 			
 			Config config = RunDrtLosAngelesScenario.prepareConfig(args);
 			config.controler().setLastIteration(0);
@@ -71,7 +71,7 @@ public class RunDrtLosAngelesScenarioTest {
 	@Test
 	public final void test2() {
 		try {			
-			String[] args = new String[] { "./scenarios/los-angeles-v1.1/input/drt/los-angeles-drt-v1.1-1pct.config.xml" };
+			String[] args = new String[] { "./scenarios/los-angeles-v1.1/input/drt/los-angeles-WSC-reduced-drt-v1.1-1pct.config.xml" };
 			
 			Config config = RunDrtLosAngelesScenario.prepareConfig(args);
 			config.controler().setLastIteration(0);
