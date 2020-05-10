b1433fc315a08b27f3fffebcfb6610f4107bea68
Huajun Chai <chaihuajun@gmail.com>
2020-05-10 13:42:07 -0700

    add prepareScenario function in ReduceScenario class. create test for testing reducescenario class.

--- src/main/java/org/matsim/prepare/ReduceScenario.java
+++ src/main/java/org/matsim/prepare/ReduceScenario.java
@@ -30,6 +30,7 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.gbl.Gbl;
 import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
 import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.router.TripStructureUtils.Trip;
@@ -41,22 +42,23 @@ import org.matsim.run.RunLosAngelesScenario;
 */
 
 public class ReduceScenario {
+
+	final static double linkBuffer = 1000.;
+	final static double personBuffer = 1000.;
+	
 	private static final Logger log = Logger.getLogger(ReduceScenario.class );
 	
 	public static void main(String[] args) {
 		
 		String planningAreaShpFile;
 		String[] argsWithoutCustomAttributes;
-
-		final double linkBuffer = 1000.;
-		final double personBuffer = 1000.;
 		
 		for (String arg : args) {
 			log.info( arg );
 		}
 		
 		if ( args.length==0 ) {
-			argsWithoutCustomAttributes = new String[] {"./scenarios/los-angeles-v1.1/input/los-angeles-v1.1-10pct.config.xml"}  ;
+			argsWithoutCustomAttributes = new String[] {"./scenarios/los-angeles-v1.1/input/los-angeles-v1.1-0.1pct.config.xml"}  ;
 			planningAreaShpFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/us/los-angeles/los-angeles-v1.0/original-data/shp-data/WSC_Boundary_SCAG/WSC_Boundary_SCAG.shp";
 			
 		} else {
@@ -76,9 +78,18 @@ public class ReduceScenario {
 				
 		Config config = RunLosAngelesScenario.prepareConfig( argsWithoutCustomAttributes ) ;
 		config.controler().setLastIteration(0);
-		Scenario scenario = RunLosAngelesScenario.prepareScenario( config ) ;
 		
 		ShapeFileUtils shpUtils = new ShapeFileUtils(planningAreaShpFile);
+		Scenario scenario = prepareScenario( config, shpUtils );		
+		
+		Controler controler = RunLosAngelesScenario.prepareControler(scenario);
+		controler.run();
+	}
+	
+	public static Scenario prepareScenario( Config config, ShapeFileUtils shpUtils ) {
+		Gbl.assertNotNull( config );
+		
+		Scenario scenario = RunLosAngelesScenario.prepareScenario( config ) ;
 		
 		Set<Id<Link>> linksToDelete = new HashSet<>();
 		for (Link link : scenario.getNetwork().getLinks().values()) {
@@ -140,14 +151,13 @@ public class ReduceScenario {
 			scenario.getPopulation().removePerson(personId);
 		}
 		
-		Controler controler = RunLosAngelesScenario.prepareControler(scenario);
-		controler.run();
-		
 		log.info("deleted persons: " + personsToDelete.size());
 		log.info("remaining persons: " + scenario.getPopulation().getPersons().size());
 
 		log.info("deleted links: " + linksToDelete.size());
 		log.info("remaining links: " + scenario.getNetwork().getLinks().size());
+		
+		return scenario;
 	}
 
 }
--- src/test/java/org/matsim/run/RunLosAngelesScenarioTest.java
+++ src/test/java/org/matsim/run/RunLosAngelesScenarioTest.java
@@ -23,10 +23,13 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.analysis.ScoreStatsControlerListener.ScoreItem;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.prepare.ReduceScenario;
+import org.matsim.prepare.drt.ShapeFileUtils;
 import org.matsim.testcases.MatsimTestUtils;
 
 /**
@@ -92,6 +95,60 @@ public class RunLosAngelesScenarioTest {
 			Assert.fail();
 		}
 
+	}
+	
+	// tests if the right agents are kept in the reduced scenario.
+	// single iteration
+	// simulated pt disabled
+	@Test
+	public final void test3() {
+		try {
+			String[] args = new String[] {"./scenarios/los-angeles-v1.1/input/los-angeles-WSC-reduced-v1.1-1pct.config.xml"};
+			
+			Config config = RunLosAngelesScenario.prepareConfig(args);
+			config.controler().setLastIteration(0);
+			config.global().setNumberOfThreads(1);
+			config.controler().setOutputDirectory( utils.getOutputDirectory() );
+			config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+			config.plans().setInputFile("../../../test/input/test-agents-for-reduced-scenario.xml");
+			config.transit().setUseTransit(false);
+			
+			ShapeFileUtils shpUtils = new ShapeFileUtils("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/us/los-angeles/los-angeles-v1.0/original-data/shp-data/WSC_Boundary_SCAG/WSC_Boundary_SCAG.shp");
+			
+			Scenario scenario = ReduceScenario.prepareScenario(config, shpUtils);
+			
+			// check if the total number of agents left is correct (# = 4)
+			Assert.assertEquals("Wrong # of agents left in the reduced scenario.", 4, scenario.getPopulation().getPersons().size());
+			
+			// check if within agent is kept (person = 10000000)
+			Assert.assertTrue("Agent traveling inside WSC is not left in the reduced scenario.", 
+					scenario.getPopulation().getPersons().containsKey(Id.createPersonId("10000000")));
+			
+			// check if out agent is kept (person = 10000001)
+			Assert.assertTrue("Agent travelling out from WSC is not left in the reduced scenario.", 
+					scenario.getPopulation().getPersons().containsKey(Id.createPersonId("10000001")));
+			
+			// check if into agent is kept (person = 10000002)
+			Assert.assertTrue("Agent travelling into WSC is not left in the reduced scenario.", 
+					scenario.getPopulation().getPersons().containsKey(Id.createPersonId("10000002")));
+			
+			// check if thru agent is kept (person = 10000003)
+			Assert.assertTrue("Agent travelling through WSC is not left in the reduced scenario.", 
+					scenario.getPopulation().getPersons().containsKey(Id.createPersonId("10000003")));
+			
+			// check if unrelated agent is removed (person = 10000005)
+			Assert.assertTrue("Agent unrelated to WSC is not removed in the reduced scenario.", 
+					!scenario.getPopulation().getPersons().containsKey(Id.createPersonId("10000005")));
+			
+			Controler controler = RunLosAngelesScenario.prepareControler(scenario);
+			controler.run();
+			
+			
+		} catch ( Exception ee) {
+			ee.printStackTrace();
+			Logger.getLogger(this.getClass()).fatal("there was an exception: \n" + ee );
+			Assert.fail();
+		}
 	}
 
 }
